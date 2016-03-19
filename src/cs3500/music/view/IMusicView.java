6e8b7e87405989de7dd46a7ef5fab3301c6ec01b
Alxe <alex@alxe.co>
2016-03-19 14:49:44 -0400

    IView interface updated

--- src/cs3500/music/view/IMusicView.java
+++ src/cs3500/music/view/IMusicView.java
@@ -9,4 +9,12 @@ public interface IMusicView {
    * Initializes this view object.
    */
   void initialize();
+
+  /**
+   * Updates the view to the given time. In the case of a midi view, plays all notes that havent
+   * been played up to the beat at or right before the given time.
+   *
+   * @param time the time
+   */
+  void update(int time);
 }
