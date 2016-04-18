2ecb7857551e9557e9d2eb444c35504a0493fdb5
Brendon Wong <wong.bre@husky.neu.edu>
2016-04-18 15:18:51 -0400

    Cleaned up IMusicModelAdapter and created a pitch conversion method

--- src/cs3500/music/model/IMusicModelToIMusicAdapter.java
+++ src/cs3500/music/model/IMusicModelToIMusicAdapter.java
@@ -52,7 +52,21 @@ public class IMusicModelToIMusicAdapter implements IMusic {
     // do nothing because this is not needed
   }
 
-  // TODO: could (WILL) encounter an issue with the way Pitches are constructed by the other group
+  /**
+   * Converts a MIDI pitch into an other.model.Pitch object. Uses some workarounds due to
+   * differences of assumptions and invariants.
+   *
+   * @param midiPitch the MIDI pitch to convert
+   * @return the Pitch translation of the given MIDI pitch
+   */
+  private static Pitch midiPitchToOtherPitch(int midiPitch) {
+    // creates a dummy pitch to mutate because the other implementation assumes different
+    // invariants and has a different scale for pitches (it's a gross but necessary workaround)
+    Pitch result = new Pitch(1);
+    result.setOctave(cs3500.music.model.Note.midiToOctave(midiPitch)); // sets the octave
+    result.setPitch(Pitch.MusicPitch.values()[midiPitch % 12]); // sets the pitch
+  }
+  
   @Override
   public TreeMap<Pitch, TreeMap<Integer, IBeat>> getCollections() {
     cs3500.music.model.Note lowestNote = this.adaptee.getLowestNote();
@@ -63,24 +77,15 @@ public class IMusicModelToIMusicAdapter implements IMusic {
     // creates the TreeMaps for the range of Pitches
     TreeMap<Pitch, TreeMap<Integer, IBeat>> result = new TreeMap<>();
     for (int i = lowestPitch; i <= highestPitch; i++) {
-      Pitch p = new Pitch(1);
-      p.setOctave(cs3500.music.model.Note.midiToOctave(i)); // sets the octave
-      p.setPitch(Pitch.MusicPitch.values()[i % 12]); // sets the pitch
-
-      result.put(p, new TreeMap<>());
+      Pitch pitch = midiPitchToOtherPitch(i);
+      result.put(pitch, new TreeMap<>());
     }
 
     Set<cs3500.music.model.Note> adapteeNotes = this.adaptee.getNotes();
 
     // creates the HeadBeat and TailBeats for each of the notes in the model
     for (cs3500.music.model.Note n : adapteeNotes) {
-
-      // creates a dummy pitch to mutate because the other implementation assumes different
-      // invariants and has a different scale for pitches
-      Pitch pitch = new Pitch(1);
-      pitch.setOctave(cs3500.music.model.Note.midiToOctave(n.getMidiPitch())); // sets the octave
-      pitch.setPitch(Pitch.MusicPitch.values()[n.getMidiPitch() % 12]); // sets the pitch
-
+      Pitch pitch = midiPitchToOtherPitch(n.getMidiPitch());
       TreeMap<Integer, IBeat> beatMap = result.get(pitch);
       int noteEnd = n.getStart() + n.getDuration() - 1;
       IBeat head = new HeadBeat(pitch, n.getStart(), noteEnd, n.getInstrument(), n.getVolume());
